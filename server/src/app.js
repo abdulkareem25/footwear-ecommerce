6cf02ef10002032b5f16bf1b1775995df2d9f866
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-04-06 16:13:27 +0530

    cors used to connect frontend

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,17 @@
 require('./config/env');
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authRouter = require('./routes/auth.routes')
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
